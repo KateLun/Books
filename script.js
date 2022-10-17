410c771c3a4adf5cf29a4a716733057fd2dc0d34
KateLun <ekat.goryacheva@mail.ru>
2022-10-17 22:41:30 +0300

    модальное окно и сохранение в LS

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
-const books = [
+let booksNumber = 1
+
+let books = [
     {
-        id: 1,
+        id: booksNumber++,
         title: 'Sapiens. Краткая история человечества',
         authors: 'Юваль Ной Харари',
         year: '2011',
         image: 'https://avatars.mds.yandex.net/get-marketpic/1617999/market_6oJ_25OKOHB8Caas2cFv-Q/orig'
     },
     {
-        id: 2,
+        id: booksNumber++,
         title: 'Homo Deus. Краткая история будущего',
         authors: 'Юваль Ной Харрари',
         year: '2015',
         image: 'https://sindbadbooks.ru/image/cache/data/2020/Homo_deus_KBS_2D_site-700x700.png'
     },
     {
-        id: 3,
+        id: booksNumber++,
         title:'Мартин Иден',
         authors: 'Джек Лондон',
         year: '1909',
         image: 'https://img-gorod.ru/28/335/2833520_detail.jpg'
     },
     {
-        id: 4,
+        id: booksNumber++,
         title:'Таинственный остров',
         authors: 'Жюль Верн',
         year: '1875',
         image:'https://main-cdn.sbermegamarket.ru/big1/hlr-system/1487831414/100024447209b0.jpg'
     },
     {
-        id: 5,
+        id: booksNumber++,
         title:'Путешествие к центру Земли',
         authors: 'Жюль Верн',
         year: '1864',
         image:'https://cdn.eksmo.ru/v2/ITD000000000304677/COVER/cover1__w820.jpg'
     },
     {
-        id: 6,
+        id: booksNumber++,
         title:'«Приключения Шерлока Холмса: Собака Баскервилей»',
         authors: 'Артур Конан Дойл',
         year: '1902',
@@ -43,6 +45,21 @@ const books = [
     }
     ]
 const container = document.getElementById('container')
+
+const addModal = document.getElementById('add-modal')
+const closeModalButton = document.getElementById('close-modal-button')
+const openModalButton = document.getElementById('open-modal-button')
+
+function closeModal() {
+    addModal.style.display = 'none'
+}
+
+function openModal() {
+    addModal.style.display = 'flex'
+}
+
+closeModalButton.addEventListener('click', closeModal)
+openModalButton.addEventListener('click', openModal)
 
 function renderBooks() {
     container.innerHTML = ''
@@ -71,14 +88,16 @@ function renderBooks() {
     })
 }
 
-//function Image(imageValue) { ДОДЕЛАТЬ ПОТОМ
-//    if (imageValue) {
-//    image = imageValue
-//    } else {
-//    image.src = 'https://rus-traktor.ru/upload/iblock/f74/f74f39dbc9b60954c926d72401adf1cc.jpg'
-//    }
-//}
-
+function getImage(imageValue) {
+    let image
+    if (imageValue) {
+        image = imageValue
+    } else {
+        image = 'https://sun4-12.userapi.com/s/v1/if1/ChvOuAKNW6TfJDNiFhVeezp5GBYJLjC9OUpjdnvqA9F3Y79XaQIndolipLz-wOHVAIt_Td4S.jpg?size=200x200&quality=96&crop=126,126,748,748&ava=1'
+    }
+    return image
+}
+ 
 function clearForms() {
     document.getElementById('title').value = ''
     document.getElementById('authors').value = ''
@@ -93,11 +112,15 @@ function deleteBook(id) {
     const bookIndex = books.indexOf(book)  // узнаем индекс книги в массиве
     books.splice(bookIndex, 1) // удаляем элемент из массива
     renderBooks() // перерисовываем список
+
+    const booksJson = JSON.stringify(books) //сохраняем в LS
+    localStorage.setItem('books', booksJson)
 }
-function addBook() {
-    const containerBook = document.getElementById('container_addBook')
-    containerBook.style.display = 'flex'
-}
+//function addBook() {
+//    const containerBook = document.getElementById('container_addBook')
+//    containerBook.style.display = 'flex'
+//}
+
 
 function saveBook() {
     const titleValue = document.getElementById('title').value 
@@ -106,18 +129,31 @@ function saveBook() {
     const imageValue = document.getElementById('image').value 
 
     const book = {
+        id: booksNumber++,
         title: titleValue,
         authors: authorsValue,
         year: yearValue,
-        image: imageValue
+        image: getImage(imageValue)
     }
 
     books.unshift(book)
     renderBooks()
     clearForms()
-    
-    const containerBook = document.getElementById('container_addBook')
-    containerBook.style.display = 'none'
+    closeModal() 
+
+    const booksJson = JSON.stringify(books)
+    localStorage.setItem('books', booksJson)
+}
+
+renderBooks()
+
+const saveBookButton = document.getElementById('save-book-button')
+saveBookButton.addEventListener('click', saveBook)
+
+const booksJson = localStorage.getItem('books')
+//const savedBooks = JSON.parse(booksJson)
+if (booksJson) {
+    books = JSON.parse(booksJson)
 }
 
 renderBooks()
