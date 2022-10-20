23ec96cfdea117b583d6e0d016c0ef5bd15f9cd6
KateLun <ekat.goryacheva@mail.ru>
2022-10-21 00:34:55 +0300

    обновление, адаптив

--- script.js
+++ script.js
@@ -61,6 +61,11 @@ function openModal() {
 closeModalButton.addEventListener('click', closeModal)
 openModalButton.addEventListener('click', openModal)
 
+function saveToLocalStorage() { //сохраняем в LS
+    const booksJson = JSON.stringify(books) //переводим объект массива в JSON
+    localStorage.setItem('books', booksJson) //передаем данные в хранилище LS
+}
+
 function renderBooks() {
     container.innerHTML = ''
     books.forEach((book) => {
@@ -78,14 +83,31 @@ function renderBooks() {
             </div>
             <div class='button-flex'>
                 <div class='button-div'>
-                    <button class='button_change button_book button'>Изменить</button>
+                    <button id='openModalUpdate-${book.id}' class='button_change button_book button'>Обновить</button>
                 </div>
                 <div class='button-div'>
-                    <button class='button_delete button_book button' onclick='deleteBook(${book.id})'>Удалить</button>
+                    <button id='deleteBookButton-${book.id}' class='button_delete button_book button'>Удалить</button>
                 </div>
             </div>
         </div> `
-    })
+    });
+
+    books.forEach((book) => {
+        document.getElementById(`deleteBookButton-${book.id}`).addEventListener('click', () => {
+            deleteBook(book.id); //удаляется та книга, на которую нажал пользователь
+        });
+    });
+
+    books.forEach((book) => { //проходимся по всему массиву книг и берем id книги, на кнопку которой нажали обновить/изменить
+        function makeOpenUpdateModal() {
+            openUpdateModal(book.id)
+        }
+        //короткая запись без функ makeOpen.. - .addEventListener('click', () => OpenUpdateModal(book.id))
+
+        document.getElementById(`openModalUpdate-${book.id}`).addEventListener('click', makeOpenUpdateModal) //вешаем обработчик, передаем не вызов функции, а саму функцию
+    });
+
+    saveToLocalStorage()
 }
 
 function getImage(imageValue) {
@@ -106,21 +128,22 @@ function clearForms() {
 }
 
 function deleteBook(id) {
-    const book = books.find((b) => {  // находим книгу
+    const bookDel = books.find((b) => {  // находим книгу по id
         return b.id === id
     })
-    const bookIndex = books.indexOf(book)  // узнаем индекс книги в массиве
-    books.splice(bookIndex, 1) // удаляем элемент из массива
+    const bookIndex = books.indexOf(bookDel)  // узнаем индекс книги в массиве,  присваиваем переменной индексы книг из массива
+    books.splice(bookIndex, 1) // удаляем 1 элемент из массива
     renderBooks() // перерисовываем список
-
-    const booksJson = JSON.stringify(books) //сохраняем в LS
-    localStorage.setItem('books', booksJson)
+    saveToLocalStorage() //сохраняем в LS
 }
+
 //function addBook() {
 //    const containerBook = document.getElementById('container_addBook')
 //    containerBook.style.display = 'flex'
 //}
 
+const saveBookButton = document.getElementById('save-book-button')
+saveBookButton.addEventListener('click', saveBook)
 
 function saveBook() {
     const titleValue = document.getElementById('title').value 
@@ -140,20 +163,68 @@ function saveBook() {
     renderBooks()
     clearForms()
     closeModal() 
-
-    const booksJson = JSON.stringify(books)
-    localStorage.setItem('books', booksJson)
+    saveToLocalStorage()
 }
 
-renderBooks()
+const updateModal = document.getElementById('update_add-modal')
+const closeUpdateModalButton = document.getElementById('close-update-modal-button')
+const updateBookButton = document.getElementById('update-book-button')
 
-const saveBookButton = document.getElementById('save-book-button')
-saveBookButton.addEventListener('click', saveBook)
+closeUpdateModalButton.addEventListener('click', closeUpdateModal)
+
+function closeUpdateModal() {
+    updateModal.style.display = 'none'
+}
+
+function openUpdateModal(id) {
+    updateModal.style.display = 'flex'
+
+    const currentBook = books.find(b => b.id === id) //находим(find) книгу: проходимся по массиву и ищем книгу, у которой id = id что мы передали при нажатии
+
+    document.getElementById('updateTitle').value = currentBook.title //находим html элемент и передаем туда значение - данные книги
+    document.getElementById('updateAuthors').value = currentBook.authors
+    document.getElementById('updateYear').value = currentBook.year
+    document.getElementById('updateImage').value = currentBook.image
+
+    //function makeUpdate() {  - равноценно с записью ниже
+    //    updateBook(id, makeUpdate)
+    //}
+
+    const makeUpdate = () => updateBook(id, makeUpdate)
+
+    updateBookButton.addEventListener('click', makeUpdate)
+}
+
+function updateBook(id, makeUpdate) {
+    updateBookButton.removeEventListener('click', makeUpdate) // удаляем слушателя обработчика события
+
+    const oldBook = books.find(b => b.id === id)
+
+    const titleValue = document.getElementById('updateTitle').value; //получаем значение из поля "имя"
+    const authorsValue = document.getElementById('updateAuthors').value; //получаем значение из поля "автор"
+    const yearValue = document.getElementById('updateYear').value; //получаем значение из поля "год"
+    const imageValue = document.getElementById('updateImage').value //*
+
+    const newBook= {
+        id: oldBook.id,
+        title: titleValue,
+        authors: authorsValue,
+        year: yearValue,
+        image: imageValue // image: document.getElementById('updateImage').value - так короче, записи равноценные, промежуточные значения* не нужны
+    }
+
+    const bookIndex = books.indexOf(oldBook) // найдем индекс (место, порядковый номер), где стояла наша старая книга
+    books.splice(bookIndex, 1, newBook) // (на какое место ставим, кол-во элементов которое удаляем, то что мы ставим на место)
+
+    renderBooks()
+    saveToLocalStorage()
+    closeUpdateModal()
+}
+
 
 const booksJson = localStorage.getItem('books')
 //const savedBooks = JSON.parse(booksJson)
 if (booksJson) {
     books = JSON.parse(booksJson)
 }
-
 renderBooks()
